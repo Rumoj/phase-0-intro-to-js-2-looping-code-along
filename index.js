b40b6f49029fd746e7c0589971f2d9a67031e1bc
joseph rumo <josephrumo660@gmail.com>
2024-07-16 23:00:11 +0300

    looping lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+//Code your solution in this file
+
+const writeCards= function(names,eventname){
+    let newArray=[]
+    for (let i=0 ; i<names.length ; i++){
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${eventname} gift!`)
+    }
+    return newArray
+
+}
+//writeCards(["Agnes","Munee","Staus"],"thank you")
+console.log(writeCards(["Agnes","Munee","Staus"],"suprise"));
+
+const countDown = function(n){
+    while(n>=0){
+        console.log(n--)
+    }
+
+}
+countDown(10);5
